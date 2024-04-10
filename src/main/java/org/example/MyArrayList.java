1f3d96ac4a24a8528ed0e2bc48ecb0917187c833
speedyjohn <nachprog@gmail.com>
2024-04-10 21:32:50 +0500

    First commit.

---
+++ src/main/java/org/example/MyArrayList.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import org.example.interfaces.MyList;
+
+import java.util.Iterator;
+
+public class MyArrayList {
+}
---
+++ src/main/java/org/example/interfaces/MyList.java
@@ -0,0 +1,38 @@
+package org.example.interfaces;
+
+public interface MyList<T> extends Iterable<T> {
+    void printArr();
+    void add(T item);
+
+    void set(int index, T item);
+
+    void add(int index, T item);
+
+    void addFirst(T item);
+
+    void addLast(T item);
+
+    void remove(int index);
+
+    void removeFirst();
+
+    void removeLast();
+
+    void sort();
+
+    T get(int index);
+
+    T getFirst();
+
+    T getLast();
+
+    int indexOf(Object object);
+
+    int lastIndexOf(Object object);
+
+    boolean exists(Object object);
+
+    public Object[] toArray();
+    void clear();
+    int size();
+}
